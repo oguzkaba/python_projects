d4406ff1b223793fd2eb226778a0279e28b2c376
Oguz KABA <oguz-kaba@hotmail.com>
2021-03-01 17:53:54 +0300

    commit13-redesign

--- ___Excel/deneme.py
+++ ___Excel/deneme.py
@@ -1,15 +1,15 @@
 #coded by oguzkaba
 
-# from openpyxl import Workbook,load_workbook
-# from openpyxl.drawing.image import Image
-# import pyqrcode as pc
+from openpyxl import Workbook,load_workbook
+from openpyxl.drawing.image import Image
+import pyqrcode as pc
 
-# # generate QR
-# a=pc.create("GV-PR-01502")
-# a.png("../fakedata/b.png",scale=3,quiet_zone=0)
-# qr_image=Image("../fakedata/b.png")
-# # QR add Exel file
-# wb = load_workbook("../fakedata/GV-PR-01502.xlsx")
+# generate QR
+a=pc.create("GV-PR-01502")
+a.png("../fakedata/b.png",scale=3,quiet_zone=0)
+qr_image=Image("../fakedata/b.png")
+# QR add Exel file
+wb = load_workbook("../fakedata/GV-PR-01502.xlsx")
 # ws = wb.active
 # # # Aktif çalışma sayfasının adını yazdırma
 # # print(wb.sheetnames)      # <Worksheet "İsimler">
@@ -37,39 +37,22 @@
 # #     wb.close()
 # # print(wb.sheetnames)   
 
-# # sheets=wb.sheetnames
-# # ws=wb[sheets[0]]
-# # ws['A49'] = 42
-# # ws['B3'] = "Merhaba"
-# # ws.append([1, 2, 3, "Merhaba", "Dünya"])  # Sıradaki satıra sırasıyla dizi elemanlarını ekler
+sheets=wb.sheetnames
+ws=wb[sheets[0]]
+ws['A49'] = 42
+ws['B3'] = "Merhaba"
+ws.append([1, 2, 3, "Merhaba", "Dünya"])  # Sıradaki satıra sırasıyla dizi elemanlarını ekler
 
 # # 1 pixel= 0.026458333 cm
 
-# qr_image.width=40
-# qr_image.height=40
-# ws.add_image(qr_image,"A49")
+qr_image.width=40
+qr_image.height=40
+ws.add_image(qr_image,"A49")
 
-# try:
-#     wb.save("../fakedata/GV-PR-01502.xlsx")
-# except:
-#     print("HATA-Dosya açık olabilir..!")
-#     wb.close()
-# print(wb.sheetnames)   
+try:
+    wb.save("../fakedata/GV-PR-01502.xlsx")
+except:
+   print("HATA-Dosya açık olabilir..!")
+   wb.close()
+print(wb.sheetnames)   
 
-# MAAŞ Zam Hesaplama
-
-eski_Banka=input("'Eski'- Bankaya yatan(AGİ dahil) :")
-yeni_Banka=input("'Yeni'- Bankaya yatan(AGİ dahil) :")
-
-elden_eski=input("'Eski'- Nakit alınan :")
-elden_yeni=input("'Yeni'- Nakit alınan :")
-
-eski_maas=float(eski_Banka)+float(elden_eski)
-yeni_maas=float(yeni_Banka)+float(elden_yeni)
-
-sonuc_tl=yeni_maas-eski_maas
-sonuc_yuzde=(sonuc_tl/eski_maas)*100
-
-print("Yapılan zam: %" +str(round(sonuc_yuzde,1))+"  ,  "+ str(sonuc_tl)+"TL" )
-print("'Yeni' maaş: "+str(yeni_maas)+" TL")
-print("'Eski' maaş: "+str(eski_maas)+" TL")
---
+++ ___Finance/zam_hesaplama.py
@@ -0,0 +1,17 @@
+# MAAŞ Zam Hesaplama
+
+eski_Banka=input("'Eski'- Bankaya yatan(AGİ dahil) :")
+yeni_Banka=input("'Yeni'- Bankaya yatan(AGİ dahil) :")
+
+elden_eski=input("'Eski'- Nakit alınan :")
+elden_yeni=input("'Yeni'- Nakit alınan :")
+
+eski_maas=float(eski_Banka)+float(elden_eski)
+yeni_maas=float(yeni_Banka)+float(elden_yeni)
+
+sonuc_tl=yeni_maas-eski_maas
+sonuc_yuzde=(sonuc_tl/eski_maas)*100
+
+print("Yapılan zam: %" +str(round(sonuc_yuzde,1))+"  ,  "+ str(sonuc_tl)+"TL" )
+print("'Yeni' maaş: "+str(yeni_maas)+" TL")
+print("'Eski' maaş: "+str(eski_maas)+" TL")
